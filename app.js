ed2a4477c75b3f53d8fa32fcd6ac49e7164829ef
shiro <9waiphyomg@gmail.com>
2023-11-08 02:57:45 +0630

    returning 404 page

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require("express")
+const express = require("express") ;
 
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser") ;
 
-const app = express()
+const app = express() ; 
+
+const path = require("path") ; 
 
 const adminRoutes = require("./routes/admin")
 const shopRoutes = require("./routes/shop")
@@ -13,7 +15,8 @@ app.use("/admin" , adminRoutes)
 app.use(shopRoutes)
 
 app.use((req, res , next) => {
-  res.status(404).send("<h1>Page Not found</h1>")
+  // res.status(404).send("<h1>Page Not found</h1>")
+  res.status(404).sendFile(path.join(__dirname , "views" , "404.html"))
 })
 
 app.listen(8000)
