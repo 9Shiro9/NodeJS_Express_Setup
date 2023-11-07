9253f2ef64c0b38ee74dc7b7dae0d656d441ddc5
shiro <9waiphyomg@gmail.com>
2023-11-08 02:53:02 +0630

    serving html pages

--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
+const path = require("path") ; 
+
 router.get("/", (req, res, next) => {
-    res.send("<h1>Hello From Express</h1>")
+    res.sendFile(path.join(__dirname , "../" , "views" , "shop.html"))
+    // res.send("<h1>Hello From Express</h1>")
  })
 
 module.exports = router;
